442fab40358ee4fb027555d136591c6f58b97d8c
Bach Nguyen <root.bachnc@gmail.com>
2024-05-14 17:51:40 +0900

    ADD stock FastAPI template

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # Put the code for your API here.
+import pickle
+import pandas as pd
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
