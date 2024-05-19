644a9a85ef4f155ba0e99079168e0da5e1404965
Bach Nguyen <root.bachnc@gmail.com>
2024-05-20 01:24:45 +0900

    ADD working predict API

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # Put the code for your API here.
 import pickle
 import pandas as pd
+import json
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from starter.ml.data import process_data
+from starter.ml.model import inference
 
 app = FastAPI()
 
@@ -9,3 +13,46 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return "Hello World"
+
+class Input(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int = Field(alias='education-num')
+    marital_status: str = Field(alias='marital-status')
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int = Field(alias='capital-gain')
+    capital_loss: int = Field(alias='capital-loss')
+    hours_per_week: int = Field(alias='hours-per-week')
+    native_country: str = Field(alias='native-country')
+
+@app.post(path="/predict")
+async def predict(input: Input):
+    [model, encoder, lb] = pickle.load(open("model/model.pkl", "rb"))
+    df = pd.DataFrame(input.dict(by_alias=True), index=[0])
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X, _, _, _ = process_data(
+        X=df,
+        categorical_features=cat_features,
+        label=None,
+        training=False,
+        encoder=encoder,
+        lb=lb
+    )
+    
+    predictions = inference(model, X)
+
+    return json.dumps(predictions.tolist())
--- test/restapi_test.py
+++ test/restapi_test.py
@@ -12,3 +12,28 @@ def test_root():
     response = client.get("/")
     assert response.text == '"Hello World"'
 
+def test_predict():
+    """
+    Test (/predict) api POST
+    """
+
+    res = client.post("/predict", 
+        json = {
+            "age": 28,
+            "workclass": "Private",
+            "fnlgt": 338409,
+            "education": "Bachelors",
+            "education-num": 13,
+            "marital-status": "Married-civ-spouse",
+            "occupation": "Prof-specialty",
+            "relationship": "Wife",
+            "race": "Black",
+            "sex": "Female",
+            "capital-gain": 0,
+            "capital-loss": 0,
+            "hours-per-week": 40,
+            "native-country": "Cuba"
+        }
+
+    )
+    assert res.text == '"[0]"'
